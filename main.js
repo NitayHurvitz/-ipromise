7926463cdc53d92e6c531b384d40e80078af63a8
Nitay Hurvitz <nitay.hurvitz@verint.com>
2020-10-10 13:40:19 +0300

    cr comments

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
-const { rejects } = require('assert');
-const { FILE } = require('dns');
 const fs = require('fs');
-const { resolve } = require('path');
-
 
 function myReadFile(filePath) {
     return new Promise((resolve, reject) => {
-        fs.readFile(filePath, (err, data) => {
+        fs.readFile(filePath, 'utf8', (err, data) => {
             if (err) {
                 reject(err)
             }
             else {
-                resolve(data.toString())
+                resolve(data)
             }
         })
     })
@@ -21,10 +17,8 @@ const FILES_TO_READ_PATH = "./filesToRead.txt"
 
 myReadFile(FILES_TO_READ_PATH)
     .then(content => content.split('\r\n'))
-    .then(paths => paths.forEach(path => {
-        myReadFile(path)
-            .then(content => console.log(`content of ${path} is: \n\t${content}\n`))
-            .catch(err => console.log(`content of ${path} is unavailable because of: \n\t${err}\n`))
-    })
+    .then(paths => Promise.all(paths.map(path => myReadFile(path)))
+        .then(content => console.log(`content is: \n\t${content}\n`))
+        .catch(err => console.log(`content is unavailable because of: \n\t${err}\n`))
     )
     .catch(err => console.log(err))
